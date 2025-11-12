7a19fffacaea4aab27870ea31dbe8023c4cd4e5f
pengcheng888 <pengcheng@example.com>
2025-11-12 10:23:15 +0800

    issue/581 - 添加linear的实现,接口和测试

--- python/infinicore/nn/functional.py
+++ python/infinicore/nn/functional.py
@@ -65,3 +65,24 @@ def swiglu(input: Tensor, other: Tensor, *, out=None):
     _infinicore.swiglu_(out._underlying, input._underlying, other._underlying)
 
     return out
+
+
+def linear(input: Tensor, weight: Tensor, bias=None, *, out=None) -> Tensor:
+    r"""Applies a linear transformation to the incoming data: y=xA^T+b."""
+
+    if out is None:
+        return Tensor(
+            _infinicore.linear(
+                input._underlying,
+                weight._underlying,
+                None if bias is None else bias._underlying,
+            )
+        )
+
+    _infinicore.linear_(
+        out._underlying,
+        input._underlying,
+        weight._underlying,
+        None if bias is None else bias._underlying,
+    )
+    return out
---
+++ src/infinicore/pybind11/ops/linear.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "infinicore/ops/linear.hpp"
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+namespace infinicore::ops {
+
+inline void bind_linear(py::module &m) {
+    m.def("linear",
+          &op::linear,
+          py::arg("input"),
+          py::arg("weight"),
+          py::arg("bias") = py::none(),
+          R"doc(Applies a linear transformation to the incoming data: y=xA^T+b.)doc");
+
+    m.def("linear_",
+          &op::linear_,
+          py::arg("out"),
+          py::arg("input"),
+          py::arg("weight"),
+          py::arg("bias") = py::none(),
+          R"doc(In-place, applies a linear transformation to the incoming data: y=xA^T+b.)doc");
+}
+
+} // namespace infinicore::ops
