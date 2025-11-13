206312fa5d14a76bb96315caa01ccc5ff99c5926
pengcheng888 <pengcheng@example.com>
2025-11-13 17:02:07 +0800

    issue/567-只处理infinicore.Tensor,能够加载infinicore.Tensor的权重,修改了module.py paramter.py部分代码

--- python/infinicore/nn/__init__.py
+++ python/infinicore/nn/__init__.py
@@ -1,3 +1,8 @@
 from infinicore.nn import (
     functional as functional,
 )
+from infinicore.nn import (
+    modules as modules,
+)
+from infinicore.nn.functional import *  # noqa: F403
+from infinicore.nn.modules import *  # noqa: F403
