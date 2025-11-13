271d287b4bf7c0204db3e0e6c6d05887e5729310
pengcheng888 <pengcheng@example.com>
2025-11-13 08:42:09 +0800

    issue/586 - 添加python的scaled_dot_product_attention的实现和测试

--- python/infinicore/nn/functional.py
+++ python/infinicore/nn/functional.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import infinicore
 from infinicore.lib import _infinicore
 from infinicore.tensor import Tensor
@@ -63,5 +65,46 @@ def swiglu(input: Tensor, other: Tensor, *, out=None):
         return Tensor(_infinicore.swiglu(input._underlying, other._underlying))
 
     _infinicore.swiglu_(out._underlying, input._underlying, other._underlying)
+
+    return out
+
+
+def scaled_dot_product_attention(
+    query: Tensor,
+    key: Tensor,
+    value: Tensor,
+    attn_mask: Optional[Tensor] = None,
+    dropout_p: float = 0.0,
+    is_causal: bool = False,
+    scale: Optional[float] = None,
+    enable_gqa: bool = False,
+    *,
+    out=None,
+) -> Tensor:
+    r"""Computes scaled dot product attention on query, key and value tensors."""
+    assert (attn_mask is None) and (0.0 == dropout_p), "Unsupported parameters."
+    assert (enable_gqa is True) and (is_causal is True), "Incorrect parameter value."
+
+    ntoken = query.shape[-2]
+    total_token = key.shape[-2]
+
+    assert (1 == ntoken and total_token > 1) or (ntoken == total_token), (
+        "Incorrect parameter value."
+    )
+
+    if out is None:
+        return infinicore.Tensor(
+            _infinicore.scaled_dot_product_attention(
+                query._underlying, key._underlying, value._underlying, scale
+            )
+        )
+
+    _infinicore.scaled_dot_product_attention_(
+        out._underlying,
+        query._underlying,
+        key._underlying,
+        value._underlying,
+        scale,
+    )
 
     return out
