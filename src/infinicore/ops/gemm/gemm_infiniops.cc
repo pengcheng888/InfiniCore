3e32a99c44f760de466ed6db1668b4518ac5afc9
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-08-06 16:47:06 +0800

    issue/1465 fix: adapt Gemm call to optional C API

--- src/infinicore/ops/gemm/gemm_infiniops.cc
+++ src/infinicore/ops/gemm/gemm_infiniops.cc
@@ -45,6 +45,7 @@ void run(void *planned_meta) {
         config,
         planned->a.tensor(planned->a_tensor),
         planned->b.tensor(planned->b_tensor),
+        std::optional<infini::rt::TensorView>{},
         std::optional<float>{planned->alpha},
         std::optional<float>{planned->beta},
         std::optional<int>{},
