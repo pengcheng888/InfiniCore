8a26fb62743b6c0c97451f5eda55eb8eb78fb7ba
wooway777 <wooway777@gmail.com>
2026-08-19 04:39:29 +0000

    refactor: remove unwanted swiglu header

--- src/infinicore/ops/swiglu/swiglu_infiniops.cc
+++ src/infinicore/ops/swiglu/swiglu_infiniops.cc
@@ -2,7 +2,12 @@
 
 #ifdef ENABLE_INFINIOPS_API
 #include "../infiniops_impl.hpp"
-#include "swiglu_infiniop.hpp"
+
+namespace infinicore::op::swiglu_impl::infiniop {
+void *plan(Tensor c, const Tensor &a, const Tensor &b);
+void run(void *planned_meta);
+void cleanup(void **planned_meta_ptr);
+} // namespace infinicore::op::swiglu_impl::infiniop
 
 namespace infinicore::op::swiglu_impl::infiniops {
 
